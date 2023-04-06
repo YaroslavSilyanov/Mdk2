75d52df62c81eea21463ab0ef25b0cd3173e4319
YaroslavSilyanov <118308426+YaroslavSilyanov@users.noreply.github.com>
2023-04-06 13:06:40 +0300

    Add files via upload

---
+++ RoomLibrary/RoomLibrary/room.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomLibrary
+{
+    public class room
+    {
+        double roomLength;
+        double roomWidth;
+        public double RoomLength
+        {
+            get { return roomLength; }
+            set { roomLength = value; }
+        }
+        public double RoomWidth
+        {
+            get { return RoomWidth; }
+            set { RoomWidth = value; }
+        }
+        /// <summary>
+        /// метод вычисляет периметр комнаты
+        /// </summary>
+        /// <returns>возвращает значение периметра</returns>
+        public double RoomPerimetr()
+        {
+            return 2 * (roomLength + RoomWidth);
+        }
+        /// <summary>
+        /// метод вычисляет площадь комнаты
+        /// </summary>
+        /// <returns>возвращает значение площади</returns>
+        public double RoomArea()
+        {
+            return roomLength * RoomWidth;
+        }
+        /// <summary>
+        /// метод вычисляет число квадратных метров на одного человека
+        /// </summary>
+        /// <param name="np"></param>
+        /// <returns>возвращает число квадратных метров</returns>
+        public double PersonArea(int np)
+        {
+            return RoomArea() / np;
+
+        }
+
+    }
+    
+}
